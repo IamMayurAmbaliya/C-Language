3f2e32f7002c4afdad1cef87a6169d8e3cc8fdbd
IamMayurAmbaliya <mayurambaliya56@gmail.com>
2023-05-17 18:42:50 +0530

    17/05/23 | ch-2

---
+++ C/MC5-1-2.C
@@ -0,0 +1,95 @@
+/*
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("name\t\t : MAYUR\n");
+	printf("age\t\t : 25\n");
+	printf("birthdate\t : 11 NOVEMBER\n");
+	printf("birthplace\t : SURAT\n");
+	printf("father name\t : VINODBHAI\n");
+	printf("mothername\t : LABHUBEN\n");
+	printf("gender\t\t : MALE\n");
+	printf("hobby\t\t : LEARNING\n");
+
+	getch();
+}
+*/
+/*
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("---------\n");
+	printf("|       |\n");
+	printf("R      |\n");
+	printf("N       |\n");
+	printf("W      |\n");
+	printf("|       |\n");
+	printf("---------\n");
+
+
+	getch();
+}
+*/
+/*
+#include<stdio.h>
+#include<conio.h>
+// #define pi 3.14
+void main()
+{
+	int r=5;
+	const float pi=3.14;
+	clrscr();
+
+	printf("AREA OF CIRCLE : %.3f",pi*r*r);
+
+	getch();
+}
+*/
+/*
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+       int l=9;
+       float b=5.4;
+       clrscr();
+
+       printf("AREA OF RECTANGLE : %.2f",l*b);
+
+       getch();
+}
+*/
+/*
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int  b=80,h=4;
+	clrscr();
+
+	printf("AREA OF TRINGLE : %d",(b*h)/2);
+
+	getch();
+}
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=500,b=3;
+    clrscr();
+
+    printf("ENTER A VALUE : %d\n",a);
+    printf("ENTER A PERCENTAGE OF INTEREST : %d\n",b);
+    printf("TOTAL INTEREST IS : %d",(a*b)/100);
+
+    getch();
+}
+
+
+
+
