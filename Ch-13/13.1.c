8adebc4ee7b2eb0f3ee1403a6c6e24afd5facad3
IamMayurAmbaliya <133774622+IamMayurAmbaliya@users.noreply.github.com>
2023-07-17 18:40:35 +0530

    Update 13.1.c
    needed things

--- Ch-13/13.1.c
+++ Ch-13/13.1.c
@@ -13,3 +13,20 @@ void main()
 	fprintf(f2p,"%s",a);
 	fclose(f2p);
 }
+
+update karelo program
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	FILE *f1p , *f2p ;
+	char a[200];
+	
+	f1p = fopen("C:\\Users\\123\\Desktop\\F1P.txt","r");
+	fscanf(f1p,"%[^\n]",&a);
+	fclose(f1p);
+	
+	f2p = fopen("C:\\Users\\123\\Desktop\\F2P.txt","w");
+	fprintf(f2p,"%s",a);
+	fclose(f2p);
+}
